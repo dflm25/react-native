bcd2a2b0c90d5960a4e4c0b171f58b60baaf58b8
Daniel Lucumi <dlucumi@coredigital.com>
2024-06-18 11:07:24 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+
+// custom components
+import Layout from './src/components/view'
+
+export default function App() {
+  return (
+    <Layout />
+  );
+}
