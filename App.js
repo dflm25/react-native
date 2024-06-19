45be084a0f31b1f07a450ba73ccac3ad551ed4f7
Daniel Lucumi <dflm25@gmail.com>
2024-06-18 19:55:16 -0500

    added nefflix lottie

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 
 // custom components
-import Layout from './src/components/view';
-import TextComponent from './src/components/text';
-import ButtonComponent from './src/components/button';
-import ImageComponent from './src/components/image';
+import Layout from "./src/components/view";
+import TextComponent from "./src/components/text";
+import ButtonComponent from "./src/components/button";
+import ImageComponent from "./src/components/image";
 
-// 
-import SplashScreen from './src/pages/splashScreen';
+//
+import SplashScreen from "./src/pages/splashScreen";
 
 export default function App() {
-  return (
-    <ImageComponent />
-  );
+  return <SplashScreen />;
 }
--- readme.md
+++ readme.md
@@ -1,8 +1,30 @@
+Node version > 20
+
+# git clone
+
+yarn install
+
+Si se genera un error como el siguiente al clonar y tratar de correr yarn start
+
+jest-haste-map: Watchman crawl failed. Retrying once with node crawler.
+Usually this happens when watchman isn't running. Create an empty `.watchmanconfig`
+
+# solucion:
+
+- watchman watch-del-all
+- watchman shutdown-server
+
 # Proyecto netflix clone
 
 # Netflix figma
+
 https://www.figma.com/community/file/928111079251292034
 https://www.figma.com/design/H6EdxwwWEAmmbhmU3RrqIs/Netflix-(Community)?node-id=0-1&t=xkRWIiqL0IVkAY44-0
 
-# lottie 
+# lottie
+
 https://lottiefiles.com/animations/netflix-logo-Rp5zKvUaFp?from=search
+
+# movie api
+
+https://www.themoviedb.org/
